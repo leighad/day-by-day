32a90c2b9d446de6aa6d0faedcd4e2b904db326f
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-07-15 15:10:47 -0700

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+Day.destroy_all
+
+user = User.create(username: "leigha", email: "leigha@me.me", password: "1234")
+
+days =  []
+5.times do 
+    date = Time.now 
+    title = Faker::Hipster.sentence
+    content = Faker::Hipster.paragraph
+    favorite = true 
+    user_id = 1
+    day = Day.create(date: date, title: title, content: content, favorite: favorite, user_id: user.id)
+    days << day
+end
